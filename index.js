c1828ce09f097208ca7b6d1583f2e2237b660eaa
AnibalDiaz <38087750+Diazcas@users.noreply.github.com>
2021-10-26 14:34:48 -0600

    subiendo2

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const app = express();
 const port = 3000;
 app.use(express.static(__dirname + '/public'))
 
-app.set('view engine', 'ejs');
-app.set('views', __dirname + '/views');
+// app.set('view engine', 'ejs');
+// app.set('views', __dirname + '/views');
 
 
 app.listen(port, () => {
